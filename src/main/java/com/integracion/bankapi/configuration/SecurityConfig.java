56fd07007303ad73557c40b60d14d5edf008c139
gonzalo.rivero <gonzalo.rivero@naranjax.com>
2020-11-11 15:55:48 -0300

    Add cors explicit config

--- src/main/java/com/integracion/bankapi/configuration/SecurityConfig.java
+++ src/main/java/com/integracion/bankapi/configuration/SecurityConfig.java
@@ -1,22 +1,39 @@
 package com.integracion.bankapi.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http    .cors()
+                .and()
                 .authorizeRequests()
                 .anyRequest()
                 .permitAll()
                 .and()
-                .cors()
-                .and()
                 .csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
