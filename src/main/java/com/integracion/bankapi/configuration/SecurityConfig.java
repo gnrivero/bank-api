166f1076a19032537163145ae6ec34ca4d680f8d
gonzalo.rivero <gonzalo.rivero@naranjax.com>
2020-11-11 16:13:52 -0300

    agrego nuevas configs para CORS

--- src/main/java/com/integracion/bankapi/configuration/SecurityConfig.java
+++ src/main/java/com/integracion/bankapi/configuration/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
